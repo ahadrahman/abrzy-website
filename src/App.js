ba8136267f22ef77ba82029e551301e4f4774fcf
Ahad Rahman <rahmanahad@ecs.vuw.ac.nz>
2021-06-26 14:23:04 +1200

    abrzy website changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src="../abrzy_logo.png" className="abrzy-logo" alt="logo" />
+          <img src="../Abrzy-69.jpg" className="App-logo" alt="logo" />
+          <p>
+            80 DIFFERENT WAYS OUT NOW!
+          </p>
+          <div className="icons">
+            <a href="https://open.spotify.com/artist/0GIxnPuJrbOyBfyYkMzJRo?si=oHZ9VyzmToutsxDCMRrUqA&dl_branch=1">
+              <img src="../spotify.png" className="spotify-logo" alt="logo" />
+            </a>
+            <a href="https://www.instagram.com/abrzy">
+              <img src="../instagram.png" className="instagram-logo" alt="logo" />
+            </a>
+          </div>
+
+
+
+        </header>
+      </div>
   );
 }
 
